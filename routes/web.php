4657e30da872ad1c3d41401326fbc36233a38d4b
Federico <fedu_zero@hotmail.com>
2022-02-11 17:49:18 -0300

    Factory Model no funciono

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,6 @@ Route::get('/',
 
  Route::get('/about',
  [PagesController::class, 'about']);
+
+ Route::get('/posts',
+ [PostsController::class, 'index']);
