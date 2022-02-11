fb31515d336f6d2f54caa175b5ceb370ca2d74eb
Federico <fedu_zero@hotmail.com>
2022-02-11 20:59:12 -0300

    Métodos útiles para Controller
    La nueva forma de usar métodos.

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -24,4 +24,67 @@ class PostFactory extends Factory
             'created_at' => now(),
         ];
     }
+
+    //este metodo viene de otra seccion, donde lo hizo con otros objeto, esto me servira para reemplazar a como lo hacia en MVC
+    public function store(Request $request){
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+
+        /*
+        Esta forma es igual que la de arriba pero debo ir al modelo Post para agregar: protected $fillable =['title','body'];
+
+        $post = Post::create([
+            'title' => $request->input('title'),
+            'body' => $request->input('body')
+
+        ]);
+
+        Si dejo la PALABRA create no es necesario usar save(), a diferencia de make que me lo pedira
+
+        */
+
+        //este save es lo que permitira que se guarde en la base de datos
+        $post->save();
+
+        return redirect('/');
+    }
+
+    public function edit(){
+        $post = Post::find($id)->first();
+
+        return view('post.create')->with('post', $post);
+    }
+
+    public function update(Request $request, $id){
+        $post = Post::where('id',$id)
+            ->update([
+                'title' => $request->input('title'),
+                'body' => $request->input('body')
+
+        ]);
+        
+        return redirect('/');
+        
+    }
+
+    public function destroy($id){
+        $post = Post::find($id)->first();
+
+        $post->delete();
+
+        return redirect('/');
+
+    }
+
+    /* Segunda forma de hacerlo
+    public function destroy(Car $car){
+        
+        $post->delete();
+
+        return redirect('/');
+
+    }
+    */
+
 }
